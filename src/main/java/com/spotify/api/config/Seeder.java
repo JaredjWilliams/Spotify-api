c30b19cd31c92ec2aa7cc09c85102fc84de774e1
jsparks9 <josiahsparks78@gmail.com>
2024-04-16 17:35:54 -0500

    Got LeaderBoard Working With Seeder Data

--- src/main/java/com/spotify/api/config/Seeder.java
+++ src/main/java/com/spotify/api/config/Seeder.java
@@ -1,9 +1,7 @@
 package com.spotify.api.config;
 
-import com.spotify.api.entitites.Credentials;
-import com.spotify.api.entitites.Profile;
-import com.spotify.api.entitites.Settings;
-import com.spotify.api.entitites.User;
+import com.spotify.api.entitites.*;
+import com.spotify.api.repositories.AttemptRepository;
 import com.spotify.api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -16,24 +14,47 @@ import java.util.*;
 //@org.springframework.context.annotation.Profile("local || test")
 public class Seeder implements CommandLineRunner {
     private final UserRepository userRepo;
+    private final AttemptRepository attemptRepo;
+    private static final Random random = new Random();
 
 
-    @Override
-    public void run(String... args) throws Exception {
+    private User createUser(String username, String password, String email, String fName) {
+        final String difficulty = "Easy";
+        final boolean showHints = true;
         Credentials creds = new Credentials();
-        creds.setUsername("test");
-        creds.setPassword("tester");
+        creds.setUsername(username);
+        creds.setPassword(password);
         Profile profile = new Profile();
-        profile.setEmail("testing@gmail.com");
-        profile.setFirstName("Test");
+        profile.setEmail(email);
+        profile.setFirstName(fName);
         Settings settings = new Settings();
-        settings.setDifficulty("Easy");
-        settings.setHints(true);
+        settings.setDifficulty(difficulty);
+        settings.setHints(showHints);
         User user = new User();
         user.setProfile(profile);
         user.setCredentials(creds);
         user.setSettings(settings);
-        userRepo.saveAndFlush(user);
+        return userRepo.saveAndFlush(user);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        final int totalAttempts = 50;
+        final int totalUsers = 15;
+        User user0 = createUser("test", "tester", "testing@gmail.com", "Test");
+        List<User> users = new ArrayList<>(List.of(user0));
+        for (int i = 0; i < totalUsers; i++)
+            users.add(createUser("test"+i, "tester", "testing" + i + "@gmail.com", "Test"+i));
+        users = userRepo.saveAllAndFlush(users);
+
+        List<Integer> scoreOptions = new ArrayList<>(Arrays.asList(5, 10, 15));
+        List<Attempt> attempts = new ArrayList<>(15);
+        for (int i = 0; i < totalAttempts; i++) {
+            Attempt attempt = new Attempt();
+            attempt.setUser(users.get(random.nextInt(users.size())));
+            attempt.setScore(scoreOptions.get(random.nextInt(scoreOptions.size())));
+            attempts.add(attempt);
+        }
+        attemptRepo.saveAllAndFlush(attempts);
     }
 }
--- src/main/java/com/spotify/api/services/impl/AttemptServiceImpl.java
+++ src/main/java/com/spotify/api/services/impl/AttemptServiceImpl.java
@@ -4,8 +4,10 @@ import com.spotify.api.dtos.AttemptRequestDto;
 import com.spotify.api.dtos.AttemptResponseDto;
 import com.spotify.api.entitites.Attempt;
 import com.spotify.api.entitites.User;
+import com.spotify.api.exceptions.NotFoundException;
 import com.spotify.api.mappers.AttemptMapper;
 import com.spotify.api.repositories.AttemptRepository;
+import com.spotify.api.repositories.UserRepository;
 import com.spotify.api.services.AttemptService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,12 +19,15 @@ public class AttemptServiceImpl implements AttemptService {
     private final AttemptMapper attemptMapper;
     private final AttemptRepository attemptRepository;
     private final UserServiceImpl userService;
+    private final UserRepository userRepository;
     @Override
     public AttemptResponseDto createAttempt(AttemptRequestDto attemptRequestDto) {
-        User user = userService.getUserByUsername(attemptRequestDto.getCredentials().getUsername());
+        final String username = attemptRequestDto.getCredentials().getUsername();
+        User user = userRepository.findByCredentialsUsernameIgnoreCase(username).orElseThrow(() -> new
+                NotFoundException("User with username '" + username + "' not found."));
         Attempt attempt = attemptMapper.requestDtoToEntity(attemptRequestDto);
         attempt.setUser(user);
 
-        return attemptMapper.entityToResponseDto(attemptRepository.saveAndFlush(attemptMapper.requestDtoToEntity(attemptRequestDto)));
+        return attemptMapper.entityToResponseDto(attemptRepository.saveAndFlush(attempt));
     }
 }

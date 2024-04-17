a9fe71c7f43d4895f5456d2e007f4384cb98a6cf
jsparks9 <66742639+jsparks9@users.noreply.github.com>
2024-04-16 20:51:57 -0500

    Disabled Blank Test

--- src/test/java/com/spotify/api/ApiApplicationTests.java
+++ src/test/java/com/spotify/api/ApiApplicationTests.java
@@ -3,10 +3,10 @@ package com.spotify.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ApiApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 

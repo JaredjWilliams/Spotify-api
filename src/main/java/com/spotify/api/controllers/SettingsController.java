36b371afb3fea655327f162d297df0d88a5b2489
jsparks9 <josiahsparks78@gmail.com>
2024-04-18 14:36:34 -0500

    Setting Persisting Fine

--- src/main/java/com/spotify/api/controllers/SettingsController.java
+++ src/main/java/com/spotify/api/controllers/SettingsController.java
@@ -3,6 +3,7 @@ package com.spotify.api.controllers;
 import com.spotify.api.dtos.SettingsDto;
 import com.spotify.api.dtos.UserResponseDto;
 import com.spotify.api.services.SettingsService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +15,12 @@ public class SettingsController {
 
     private final SettingsService settingsService;
 
-    @PatchMapping("/{username}")
+    @PutMapping("/{username}")
     @ResponseStatus(HttpStatus.OK)
-    public UserResponseDto updateSettings(@PathVariable("username") String username, @RequestBody SettingsDto settingsDto) {
+    public UserResponseDto updateSettings(
+            @PathVariable("username") String username,
+            @RequestBody @Valid SettingsDto settingsDto
+    ) {
         return settingsService.updateSettings(username, settingsDto);
     }
 }
